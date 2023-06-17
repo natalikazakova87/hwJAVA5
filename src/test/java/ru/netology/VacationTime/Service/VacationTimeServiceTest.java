deea6a1ecfef1cc31529a68d327e73f6cdf75b65
natalikazakova <natalikazakova87@gmail.com>
2023-06-17 21:53:07 +0700

    initial commit

--- src/test/java/ru/netology/VacationTime/Service/VacationTimeServiceTest.java
+++ src/test/java/ru/netology/VacationTime/Service/VacationTimeServiceTest.java
@@ -1,21 +1,17 @@
 package ru.netology.VacationTime.Service;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class VacationTimeServiceTest {
-    @Test
-    public void VacationTimeService() {
+    @ParameterizedTest
+    @CsvSource({
+            "10000, 3000, 20000, 6",
+            "100000, 60000, 150000, 5"
+    })
+    public void VacationTimeService(int income, int expenses, int threshold, int expected) {
         VacationTimeService service = new VacationTimeService();
-
-        int count = 0;
-        int money = 0;
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold = 20_000;
-        int expected = 6;
-
         int actual = service.calculate(income, expenses, threshold);
-
         Assertions.assertEquals(expected, actual);
     }
 }
