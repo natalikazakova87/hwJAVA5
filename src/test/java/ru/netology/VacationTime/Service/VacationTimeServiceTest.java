9a12ee4ff5b6edb86fe10b32eaca0168428d57bd
natalikazakova <natalikazakova87@gmail.com>
2023-06-17 22:18:36 +0700

    initial commit

--- src/test/java/ru/netology/VacationTime/Service/VacationTimeServiceTest.java
+++ src/test/java/ru/netology/VacationTime/Service/VacationTimeServiceTest.java
@@ -6,12 +6,12 @@ import org.junit.jupiter.params.provider.CsvSource;
 public class VacationTimeServiceTest {
     @ParameterizedTest
     @CsvSource({
-            "10000, 3000, 20000, 6",
-            "100000, 60000, 150000, 5"
+            "10000, 3000, 20000, 3",
+            "100000, 60000, 150000, 2"
     })
     public void VacationTimeService(int income, int expenses, int threshold, int expected) {
         VacationTimeService service = new VacationTimeService();
-        int actual = service.calculate(income, expenses, threshold);
+        int actual = service.calculate(threshold, expenses, income);
         Assertions.assertEquals(expected, actual);
     }
 }
